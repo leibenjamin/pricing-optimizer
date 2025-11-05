cc9614b522966cc27188f1fad58a1af1f146f275
benlei <lei.benjamin@gmail.com>
2025-11-05 16:32:50 -0500

    fix(pwa): export PWAUpdater and silence unused params; satisfy react-refresh rule

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { registerSW } from "virtual:pwa-register";
+import { useRegisterSW } from 'virtual:pwa-register/react'
+
+export function PWAUpdater() {
+  useRegisterSW({
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    onRegisteredSW(_swUrl, _reg) {
+      // noop
+    },
+    onNeedRefresh() {
+      // show a toast/button that calls updateServiceWorker()
+    },
+    onOfflineReady() {
+      // show “ready to work offline”
+    },
+  })
+  return null
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <PWAUpdater />
   </StrictMode>,
 )
-
-registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {},
-});
